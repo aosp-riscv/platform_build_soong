f336ca3ff4775c61fbbdcda895d50e78dd5dd013
Chen Wang <wangchen20@iscas.ac.cn>
2021-11-24 14:17:29 +0800

    pass build with "mmm bionic/libc/ --skip-soong-tests"

--- scripts/strip.sh
+++ scripts/strip.sh
@@ -98,9 +98,10 @@ do_strip_keep_mini_debug_info_linux() {
     "${CLANG_BIN}/llvm-strip" --strip-all --keep-section=.ARM.attributes --remove-section=.comment "${infile}" -o "${outfile}.tmp" || fail=true
 
     if [ -z $fail ]; then
-        "${CREATE_MINIDEBUGINFO}" "${infile}" "${outfile}.mini_debuginfo.xz"
-        "${CLANG_BIN}/llvm-objcopy" --add-section .gnu_debugdata="${outfile}.mini_debuginfo.xz" "${outfile}.tmp"
-        rm -f "${outfile}.mini_debuginfo.xz"
+        echo "TBD: CREATE_MINIDEBUGINFO has NOT been supported!!!!!!!"
+        #"${CREATE_MINIDEBUGINFO}" "${infile}" "${outfile}.mini_debuginfo.xz"
+        #"${CLANG_BIN}/llvm-objcopy" --add-section .gnu_debugdata="${outfile}.mini_debuginfo.xz" "${outfile}.tmp"
+        #rm -f "${outfile}.mini_debuginfo.xz"
     else
         cp -f "${infile}" "${outfile}.tmp"
     fi
